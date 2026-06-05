d496de3ef2e8ec2c19e08e2520f192b83f981655
Bangmaster <feelthepomp@gmail.com>
2026-06-05 23:09:03 +0200

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://hcbgxmrhjvluzxnactbf.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhjYmd4bXJoanZsdXp4bmFjdGJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2ODQ1ODIsImV4cCI6MjA5NjI2MDU4Mn0.Nrz6g3kUAk5twyPQZBADxIHg6rc9Y-OZHM_37p6whvg'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
