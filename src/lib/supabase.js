bf0ddffe5d9738f7665cb6329ee08ff5b24bb9c0
Bangmaster <feelthepomp@gmail.com>
2026-09-06 00:35:39 +0200

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://hcbgxmrhjvluzxnactbf.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhjYmd4bXJoanZsdXp4bmFjdGJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2ODQ1ODIsImV4cCI6MjA5NjI2MDU4Mn0.Nrz6g3kUAk5twyPQZBADxIHg6rc9Y-OZHM_37p6whvg'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://qsykmhtnhpbzbkxyiwji.supabase.co'
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzeWttaHRuaHBiemJreHlpd2ppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg2Mzk1MTYsImV4cCI6MjEwNDIxNTUxNn0.imjqnIsFkQhY4hA_1ErhLx7ygrVxm7N0FqL5Ef0JwBI'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
